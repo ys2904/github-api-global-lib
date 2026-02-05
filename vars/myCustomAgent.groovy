930d0ec6cd083a3f79a7516174b16155d5a1ba42
caffreyneal541@gmail.com <166401482+ys2904@users.noreply.github.com>
2026-02-05 15:24:46 +0530

    Remove DEBUG flag from podTemplate call

--- vars/myCustomAgent.groovy
+++ vars/myCustomAgent.groovy
@@ -1,6 +1,6 @@
 def call(Closure body) {
     def podYaml = libraryResource 'my_pod.yaml'
-    podTemplate(yaml: podYaml, showRawYaml: DEBUG) {
+    podTemplate(yaml: podYaml) {
         node(POD_LABEL) {
             body.call() // Executes the pipeline stages defined in the Jenkinsfile
         }
