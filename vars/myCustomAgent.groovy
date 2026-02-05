bd4518182e9b126ae920eee319deed3c809416ad
caffreyneal541@gmail.com <166401482+ys2904@users.noreply.github.com>
2026-02-05 15:14:57 +0530

    Fix pod YAML resource path in myCustomAgent

--- vars/myCustomAgent.groovy
+++ vars/myCustomAgent.groovy
@@ -1,5 +1,5 @@
 def call(Closure body) {
-    def podYaml = libraryResource 'k8s/my_pod.yaml'
+    def podYaml = libraryResource 'my_pod.yaml'
     podTemplate(yaml: podYaml) {
         node(POD_LABEL) {
             body.call() // Executes the pipeline stages defined in the Jenkinsfile
